6cc0e1428d2174d440dc3311159b6327d5139da3
scottdiemer <sdiemer@cfl.rr.com>
2020-08-17 16:20:03 -0400

    Added ActionCreators and Reducers for checkout process

--- src/data/RestDataSource.js
+++ src/data/RestDataSource.js
@@ -4,5 +4,8 @@ import { RestUrls } from "./Urls";
 export class RestDataSource {
   GetData = async (dataType, params) =>
     this.SendRequest("get", RestUrls[dataType], params);
-  SendRequest = (method, url, params) => Axios.request({ method, url, params });
+  StoreData = (dataType, data) =>
+    this.SendRequest("post", RestUrls[dataType], {}, data);
+  SendRequest = (method, url, params, data) =>
+    Axios.request({ method, url, params, data });
 }
